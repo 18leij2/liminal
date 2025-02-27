f07c417d55277c75db985c9d0617fb569c0ab4bc
heatherah64 <hhernandez@gatech.edu>
2025-02-27 02:25:28 -0500

    Added Level2 scene, light flicker script

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    public float minFlickerSpeed;
+    public float maxFlickerSpeed;
+    public float timer;
+    float interval;
+
+    void Start() {
+        timer = 0f;
+        interval = Random.Range(minFlickerSpeed, maxFlickerSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > interval){
+            if (this.gameObject.GetComponent<Light>().intensity == 0f) {
+                this.gameObject.GetComponent<Light>().intensity = Random.Range(0f, 1f);
+            } else {
+                this.gameObject.GetComponent<Light>().intensity = 0f;
+            }
+            timer = 0f;
+            interval = Random.Range(minFlickerSpeed, maxFlickerSpeed);
+        }
+
+        // this.gameObject.SetActive(true);
+        // yield WaitForSeconds(Random.Range(minFlickerSpeed, maxFlickerSpeed));
+        // this.gameObject.SetActive(false);
+        // yield WaitForSeconds(Random.Range(minFlickerSpeed, maxFlickerSpeed));
+    }
+}
