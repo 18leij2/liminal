a7ce4be925f4f9d3356c38aeef9ffe89201fa1ac
18leij2 <jasonleiqi@gmail.com>
2025-03-09 23:13:06 -0400

    Update Item.cs

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class Item : ScriptableObject
 {
     public Sprite sprite;
-    public string itemName;
     // public SlotTag itemTag;
 
     // [Header("If the item can be equipped")]
