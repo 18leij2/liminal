fa51eb10363f78b327e3c962ecb73aa0c6d9fe03
18leij2 <jasonleiqi@gmail.com>
2025-03-10 00:46:32 -0400

    refactored demo scene with ai enemy + UI

---
+++ Assets/CompleteLevel3.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CompleteLevel3 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Level 4");
+        }
+    }
+}
